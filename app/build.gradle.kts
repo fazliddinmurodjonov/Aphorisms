2cd028d60c669b5eca2c24ec4a4931bce2bf2df7
Fazliddin Murodjonov <murodjonovfazliddin1999@gmail.comgit config user.emailgit config --global user.email murodjonovfazliddin1999@gmail.com>
2024-06-14 18:44:49 +0500

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         val properties = Properties()
